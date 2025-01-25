f7a06567b8ad9f5ce31ea631cbfd42b4b5785724
Muhammad Ali Khan <ak098boy@gmail.com>
2025-01-25 22:41:35 +0500

    Update app.js
    For Rock Paper Scissors Project

--- app.js
+++ app.js
@@ -1,75 +1,69 @@
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let winnerMsg = document.querySelector(".win-Msg")
-let drawMsg = document.querySelector("#draw-Msg");
-let winMsgold = winnerMsg.innerText;
-let count = 0;
-let turn0 = true;
-let winningArr = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
-];
-const disableBoxes = () => {
-    for(box of boxes){
-        box.disabled = true;
+let userWin = 0;
+let compWin = 0;
+let playerScore = document.querySelector("#player-score");
+let compScore = document.querySelector("#comp-score");
+let msgBox = document.querySelector(".game-msg")
+let choices = document.querySelectorAll(".box");
+
+let getCompChoice = () => {
+    possibleChoices = ["rock", "paper", "scissor"];
+    let getIndex = Math.floor(Math.random() * 3);
+    return possibleChoices[getIndex];
+}
+let winMsg = (userWin) => {
+
+}
+let drawGame = (userChoice, compChoice) => {
+    msgBox.style.width = "40vw";
+    msgBox.innerText = `Draw! Player(${userChoice}) same as Comp(${compChoice})`;
+    msgBox.style.backgroundColor = "grey";
+}
+let userWinMsg = (userChoice, compChoice) => {
+    userWin++;
+    playerScore.innerText = userWin;
+    msgBox.style.width = "40vw";
+    msgBox.innerText = `You Won! Player(${userChoice}) beats Comp(${compChoice})`;
+    msgBox.style.backgroundColor = "green";
+}
+let compWinMsg = (userChoice, compChoice) => {
+    compWin++;
+    compScore.innerText = compWin;
+    msgBox.style.width = "40vw";
+    msgBox.innerText = `You Lost! Comp(${compChoice}) beats Player(${userChoice})`;
+    msgBox.style.backgroundColor = "red";
+}
+let showWin = (isWin, userChoice, compChoice) => {
+    if(isWin){
+        userWinMsg(userChoice, compChoice);
+    }
+    else {
+        compWinMsg(userChoice, compChoice);
     }
 }
-boxes.forEach((box) => {
-    box.addEventListener("click" , () => {
-        if(turn0){
-            box.innerText = "O"
-            box.style.color = "#111D4A"
-            turn0 = false;
-            count++;
+let checkWin = (userChoice, compChoice) => {
+    if(userChoice === compChoice)
+        drawGame(userChoice, compChoice);
+    else {
+        let isWin = true;
+        if(userChoice === "rock"){
+            isWin = compChoice === "paper" ? false : true;
+        }
+        else if(userChoice === "paper"){
+            isWin = compChoice === "scissor" ? false : true;
         }
         else {
-            box.innerText = "X";
-            box.style.color = "#6B0504";
-            turn0 = true;    
-            count++;
+            isWin = compChoice === "rock" ? false : true;
         }
-        box.disabled = true;
-        checkWin();
-    });
-    
-});
-
-resetBtn.addEventListener("click" , () => {
-    boxes.forEach((box) => {
-        box.disabled = false;
-        box.innerText = "";
-        console.clear();
-        winnerMsg.style.display = "none";
-        winnerMsg.innerText = winMsgold
-        drawMsg.style.display = "none";
-        count = 0;
-    });
-});
-const checkWin = () => {
-    let isWin = false;
-    for(pattern of winningArr) {
-        let pos1 = boxes[pattern[0]].innerText;
-        let pos2 = boxes[pattern[1]].innerText;
-        let pos3 = boxes[pattern[2]].innerText;
-
-        if(pos1 != "" && pos2 != "" && pos3!= "") {
-            if(pos1 === pos2 && pos2 === pos3 && pos3 === pos1){
-                winnerMsg.innerText = winnerMsg.innerText + ` is ${pos1}`;
-                winnerMsg.style.display = "block";
-                disableBoxes();
-                isWin = true;
-                break;
-            }
-            }
-        }  
-        if (!isWin && count === 9)
-            drawMsg.style.display = "block"; 
+        showWin(isWin, userChoice, compChoice);
     }
+} 
 
-
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        let userChoice = choice.getAttribute("id")
+        console.log(userChoice);
+        let compChoice = getCompChoice();
+        console.log(compChoice)
+        checkWin(userChoice, compChoice);
+    });
+});
