bb749a48612d4e962326d94c63ee96d39d644e9f
Muhammad Ali Khan <ak098boy@gmail.com>
2025-01-27 19:46:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,59 @@
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let winnerMsg = document.querySelector(".win-Msg")
-let drawMsg = document.querySelector("#draw-Msg");
-let winMsgold = winnerMsg.innerText;
-let count = 0;
-let turn0 = true;
-let winningArr = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
-];
-const disableBoxes = () => {
-    for(box of boxes){
-        box.disabled = true;
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const dropDown = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("button");
+const msg = document.querySelector(".msg-rate");
+
+
+for ( let select of dropDown){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "GBP") {
+            newOption.selected = "selected";
+        } else if(select.name === "to" && currCode === "PKR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
     }
+    select.addEventListener("change" , (evt) => {
+        upDateFlag(evt.target);
+    });
+}   
+
+const upDateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;   
 }
-boxes.forEach((box) => {
-    box.addEventListener("click" , () => {
-        if(turn0){
-            box.innerText = "O"
-            box.style.color = "#111D4A"
-            turn0 = false;
-            count++;
-        }
-        else {
-            box.innerText = "X";
-            box.style.color = "#6B0504";
-            turn0 = true;    
-            count++;
-        }
-        box.disabled = true;
-        checkWin();
-    });
-    
-});
 
-resetBtn.addEventListener("click" , () => {
-    boxes.forEach((box) => {
-        box.disabled = false;
-        box.innerText = "";
-        console.clear();
-        winnerMsg.style.display = "none";
-        winnerMsg.innerText = winMsgold
-        drawMsg.style.display = "none";
-        count = 0;
-    });
-});
-const checkWin = () => {
-    let isWin = false;
-    for(pattern of winningArr) {
-        let pos1 = boxes[pattern[0]].innerText;
-        let pos2 = boxes[pattern[1]].innerText;
-        let pos3 = boxes[pattern[2]].innerText;
-
-        if(pos1 != "" && pos2 != "" && pos3!= "") {
-            if(pos1 === pos2 && pos2 === pos3 && pos3 === pos1){
-                winnerMsg.innerText = winnerMsg.innerText + ` is ${pos1}`;
-                winnerMsg.style.display = "block";
-                disableBoxes();
-                isWin = true;
-                break;
-            }
-            }
-        }  
-        if (!isWin && count === 9)
-            drawMsg.style.display = "block"; 
+const upDateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input")
+    let amntValue = amount.value;
+    if(amntValue === "" || amntValue < 0) {
+        amntValue = 1;
+        amount.value = "1";
     }
+    console.log(fromCurr.value , toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    
+    let finalAmount = amntValue * rate;
+    msg.innerText = `${amntValue} ${fromCurr.value} = ${Math.round(finalAmount * 100)/ 100} ${toCurr.value}`;
+    console.log(finalAmount);
+}
 
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    upDateExchangeRate();
+});
 
+window.addEventListener("load", () => {
+    upDateExchangeRate();
+})
