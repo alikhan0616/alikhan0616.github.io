312f9a034e81b56634f01255a24daecdef82b303
Muhammad Ali Khan <ak098boy@gmail.com>
2025-01-29 21:18:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,52 @@
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let winnerMsg = document.querySelector(".win-Msg")
-let drawMsg = document.querySelector("#draw-Msg");
-let winMsgold = winnerMsg.innerText;
-let count = 0;
-let turn0 = true;
-let winningArr = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
-];
-const disableBoxes = () => {
-    for(box of boxes){
-        box.disabled = true;
-    }
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=faca367ec717d39fbb76e0c4b9fd542d&units=metric";
+const searchButton = document.querySelector("button");
+const userInput = document.querySelector("input");
+const errorMsg = document.querySelector(".error-msg");
+searchButton.addEventListener("click", () => {
+    let userInputValue = getUserInput();  
+    getWeather(userInputValue);
+});
+function getUserInput() {
+    return userInput.value;
 }
-boxes.forEach((box) => {
-    box.addEventListener("click" , () => {
-        if(turn0){
-            box.innerText = "O"
-            box.style.color = "#111D4A"
-            turn0 = false;
-            count++;
+function setName(data) {
+    document.querySelector(".city").innerHTML = data.name;
+}
+function setTemp(data) {
+    document.querySelector(".temp-value").innerHTML = `${Math.round(data.main.temp)}°C`;
+}
+function setImage(data) {
+let imageLink = document.querySelector(".weather-img");
+imageUrl = data.weather[0].main.toLowerCase();
+imageLink.src = `${imageUrl}.png`;
+}
+function setHumidity(data) {
+    document.querySelector("#humidity-up").innerHTML = `${data.main.humidity}%`;
+}
+function setWindSpeed(data) {
+    document.querySelector("#wind-up").innerHTML = `${data.wind.speed} km/h`;
+}
+function displayOn () {
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".temprature").style.display = "block";
+    document.querySelector(".reports").style.display = "flex";
+}
+ async function getWeather(userInputValue) {
+        const respone = await fetch(`${URL}${userInputValue}${API_KEY}`);
+        if(respone.status == 404){
+            errorMsg.style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+            document.querySelector(".temprature").style.display = "none";
+            document.querySelector(".reports").style.display = "none";
+
         }
-        else {
-            box.innerText = "X";
-            box.style.color = "#6B0504";
-            turn0 = true;    
-            count++;
-        }
-        box.disabled = true;
-        checkWin();
-    });
-    
-});
-
-resetBtn.addEventListener("click" , () => {
-    boxes.forEach((box) => {
-        box.disabled = false;
-        box.innerText = "";
-        console.clear();
-        winnerMsg.style.display = "none";
-        winnerMsg.innerText = winMsgold
-        drawMsg.style.display = "none";
-        count = 0;
-    });
-});
-const checkWin = () => {
-    let isWin = false;
-    for(pattern of winningArr) {
-        let pos1 = boxes[pattern[0]].innerText;
-        let pos2 = boxes[pattern[1]].innerText;
-        let pos3 = boxes[pattern[2]].innerText;
-
-        if(pos1 != "" && pos2 != "" && pos3!= "") {
-            if(pos1 === pos2 && pos2 === pos3 && pos3 === pos1){
-                winnerMsg.innerText = winnerMsg.innerText + ` is ${pos1}`;
-                winnerMsg.style.display = "block";
-                disableBoxes();
-                isWin = true;
-                break;
-            }
-            }
-        }  
-        if (!isWin && count === 9)
-            drawMsg.style.display = "block"; 
-    }
-
-
+        else errorMsg.style.display = "none";
+    let data = await respone.json();
+    setName(data);
+    setTemp(data);
+    setImage(data);
+    setHumidity(data);
+    setWindSpeed(data);
+    displayOn();
+ }
